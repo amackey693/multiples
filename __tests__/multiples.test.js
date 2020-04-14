ca2e99c529a6a20681a9d2b48dde8237d29f17b5
Allison Mackey <a.mackey693@gmail.com>
2020-04-14 13:37:57 -0700

    fail test for number being less than 500500

--- __tests__/multiples.test.js
+++ __tests__/multiples.test.js
@@ -6,4 +6,7 @@ describe('mutiples', () => {
   test('function returns a number', ()=>{
     expect(typeof multiples()).toEqual('number');
   })
+  test('function returns a number less then sum of all numbers', ()=>{
+    expect(multiples()).toBeLessThan(500500);
+  })
 })
--- js/multiples.js
+++ js/multiples.js
@@ -1,4 +1,7 @@
 export let multiples =()=>{
-  let number = 0;
+  let number = 1;
+  for(let  i=1; i<=1000; i++){
+    number += i
+  }
   return number;
 }
